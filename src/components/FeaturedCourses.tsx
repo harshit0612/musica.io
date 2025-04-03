d24f0d8922a7fce855ee89d247dac4978f6d2a08
harshit51046 <harshitkatiyar817@gmail.com>
2025-04-03 00:04:52 +0000

    navbar&herosection

---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,20 @@
+import courseData from '@/data/music_courses.json'
+
+export default function FeaturedCourses(){
+    return(
+        <div className='py-12 bg-gray-900'>
+            <div>
+                <h3>
+                    Featured Courses
+                </h3>
+                <h1>
+                    Learn With the Best
+                </h1>
+            </div>
+
+            <div></div>
+
+            <div></div>
+        </div>
+    )
+} 
